5ddfe5f5208ecedb5d6de08536b97a45d65470cf
Viktor <midyukinv@mail.ru>
2023-04-13 13:38:08 +0300

    add function

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 const inputRecord = document.querySelector(".inputRecord"); // получили импут с текстом
 const addRecord = document.querySelector(".addRecord"); // получили кнопку добавить
 const listContainer = document.querySelector(".listContainer"); //получили ul контэнер для задач 
-
+const form = document.querySelector("#form");
 
 let toDoList = []; //пустой масив для хранения обьектов newTodo
 
@@ -11,25 +11,55 @@ let preservation = (e) => {
   const newTodo = {
     todo: inputRecord.value,
     checked: false,
-    important: false,
+    id: false,
   };
   toDoList.push(newTodo);
-  caseKeeping();
+  paintTasc(toDoList);
 };
 
 //функция добавление дел в ul как li
-const caseKeeping = () => {
-  let displayMessage = "";
-  toDoList.forEach(function (item, index) {
-    displayMessage += `
-    <li class="checked" >
-    <input type="checkbox" id"item_${index}">
-    <label for="item_${index}">${item.todo}</label>   
-    </li>
-    `;
-    listContainer.innerHTML = displayMessage;
+// const caseKeeping = () => {
+//   let displayMessage = "";
+//   toDoList.forEach(function (item, index) {
+//     displayMessage += `
+//     <li class="checked" >
+//     <input type="checkbox" id"item_${index}">
+//     <label for="item_${index}">${item.todo}</label>   
+//     <button class="delete">❌</button>
+//     </li>
+//     `;
+//     listContainer.innerHTML = displayMessage;
+//   });
+// };
+
+// создание li в ul 
+const creatingATask = (obj) => {
+  let displayMessage = document.createElement("li");
+  displayMessage.innerHTML =
+    `
+    <input type="checkbox" id="index">
+    <label for="index">${obj.todo}</label>   
+    <button class="delete">❌</button>
+    `
+  return displayMessage;
+}
+// функция перебора и отрисовки li  в  listContainer
+function paintTasc() {
+  toDoList.innerHTML = "";
+  toDoList.forEach(function (elem) {
+    listContainer.append(creatingATask(elem));
+   
   });
-};
+   
+}
+
+
+paintTasc(toDoList);
+
+
+
+
 
 //прослушки
+//form.addEventListener("click", preservation);
 addRecord.addEventListener("click", preservation);
